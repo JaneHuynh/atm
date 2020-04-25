9fd09d656c76ef184fdf6a67042410be4904c2ba
Kim Nguyen <43408755+nguyenxuongkim2103@users.noreply.github.com>
2020-04-26 01:41:45 +0300

    class customer and class account

---
+++ main.cpp
@@ -0,0 +1,31 @@
+//=============================================================================
+//	APPLICATION:	atm2
+//	MODULE:			main
+//	PURPOSE:
+//	AUTHOR(S):		metal
+//
+//	4/25/2020 7:13:38 PM	Created.
+//=============================================================================
+#include <iostream>
+#include "Account.h"
+#include "Customer.h"
+using namespace std;
+//-----------------------------------------------------------------------------
+//	THE MAIN ENTRY POINT TO THE PROGRAM.
+//
+//	input:	argc	The number of arguments of the programme.
+//			argv	The vector of arguments of the programme.
+//	return:	The code of the reason the process was terminated for.
+//			The value defaults to zero.
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	Customer kim{ 123, "Kim" };
+	Account kim1{ 1111, "123456", 6500.0 };
+	Account kim2{ 2222, "123654", 6.0 };
+	kim.addAccount(kim1);
+	kim.addAccount(kim2);
+	cout << kim << '\n';
+
+	return 0;
+}
